e9211b27fef10b94170fac6fad163eccfdba3d41
Sascha Heyer <heyersaschastud@googlemail.com>
2020-10-04 20:31:02 +0200

    initial release

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Simple API Management Node.js Middleware
+![Simple API Management Logo](https://storage.googleapis.com/simple-api-management-assets/logo.svg) 
+# Simple API Management Node.js middleware
+
+
+
+## Installation
+
+This is a [Node.js](https://nodejs.org/en/) module available through the
+[npm registry](https://www.npmjs.com/).
+
+```bash
+$ npm install simple-api-management-nodejs
+```
+
+## Usage
+
+```js
+const simpleAPIManagement = require('simple-api-management-nodejs');
+
+var options = {
+  apiKey: 'add your API key here',
+  // [OPTIONAL] 
+  // identifier can be used to apply rate limits for specific attributes like ip or user
+  identifier: function (req, res) { 
+    if (req.user) {
+      return req.user.id;
+    }
+    return undefined;
+  },
+};
+
+app.use(simpleAPIManagement(options));
+```
---
+++ index.js
@@ -0,0 +1,77 @@
+const axios = require('axios').default;
+
+const instance = axios.create({
+    baseURL: 'https://api.simpleapimanagement.com'
+});
+
+function SimpleAPIManagement(options) {
+
+    if (options.rateLimits === undefined) {
+        options.rateLimits = true;
+    }
+
+    if (options.metrics === undefined) {
+        options.metrics = true;
+    }
+
+
+    async function simpleAPIManagementMiddleware(req, res, next) {
+        try {
+            req._startTime = Date.now();
+
+            if (options.rateLimits) {
+                let response = await instance.post('/ratelimits/validate', {
+                    path: req.path,
+                    key: options.apiKey,
+                    identifier: options.identifier,
+                    method: req.method
+                })
+
+                if (response.data.limitReached) {
+                    res.status(429).send('Rate limit exceeded')
+                }
+                else {
+                    next()
+                }
+            }
+
+
+            res.on('finish', function () {
+                if (options.metrics) {
+                    let responseTime = new Date() - req._startTime;
+                    let host = req.headers.host || req.hostname;
+                    let statusCode = res.statusCode ? res.statusCode : 000;
+
+                    let path = req.route ? req.baseUrl + req.route.path : req.baseUrl
+
+                    instance.post('/metrics', {
+                        key: options.apiKey,
+                        host: host,
+                        originalUrl: req.originalUrl,
+                        path: path,
+                        method: req.method,
+                        statusCode: statusCode,
+                        latency: responseTime
+                    })
+                }
+            })
+
+
+        }
+        catch (err) {
+            if (err.response) {
+                console.error(err.response.data);
+            } else if (err.request) {
+                console.error('Service not available');
+            }
+
+            // call next without err to prevent side effects on customers API
+            next();
+        }
+
+    }
+
+    return simpleAPIManagementMiddleware;
+}
+
+module.exports = SimpleAPIManagement;
